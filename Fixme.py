8ff2152c8e1328f254dcbbd5f737412d8c56bdb1
npcrites <nicholascrites22@gmail.com>
2023-01-29 10:04:40 -0800

    fixing linter stuff

--- Fixme.py
+++ Fixme.py
@@ -6,6 +6,20 @@ and not for loops or list comprehensions.
 '''
 
 def evens(n):
+    '''
+    Returns a list of even numbers from 0 to n inclusive.
+    >>> evens(10)
+    [0, 2, 4, 6, 8, 10]
+    >>> evens(11)
+    [0, 2, 4, 6, 8, 10]
+    >>> evens(0)
+    [0]
+    >>> evens(1)
+    [0]
+    >>> evens(-1)
+    []
+    ''' 
+    
     if n > 0:
         xs = filter(lambda n: n%2 == 0, range(n+1))
         print(list(xs))
@@ -20,6 +34,20 @@ def evens(n):
 
 
 def threes(n):
+    '''
+    Returns a list of all numbers from 0 to n inclusive that contain the digit 3.
+    >>> threes(2)
+    []
+    >>> threes(3)
+    [3]
+    >>> threes(10)
+    [3]
+    >>> threes(20)
+    [3, 13]
+    >>> threes(50)
+    [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
+    '''
+
     xs1 = filter(lambda n: '3' in str(n), range(n+1))
     print(list(xs1))
 
@@ -27,16 +55,23 @@ def threes(n):
 def small_words(text):
     '''
     Returns a list of all words in the input text that are less than 5 characters long.
-
     HINT:
     Recall that text.split() converts the text variable into a list of words.
+    >>> small_words('this is a simple test case')
+    ['this', 'is', 'a', 'test', 'case']
+    >>> small_words('really enormous words')
+    []
+    >>> small_words('')
+    []
+    >>> small_words('a big word is bad')
+    ['a', 'big', 'word', 'is', 'bad']
     '''
 
     xs2 = filter(lambda text: len(text) < 5 ,text.split())
     print(list(xs2))
 
-def squares(n):
 
+def squares(n):
     '''
     Returns a list of all square number between 1 and n inclusive.
     Recall that the nth square number is defined to be n*n.
@@ -50,9 +85,10 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
-
-    xs3 = map(lambda n: n**2, range(n+1))
+ 
+    xs3 = map(lambda n: n**2, range(1,n+1))
     print(list(xs3))
+
 
 def lengths(strings):
     '''
